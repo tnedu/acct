84ca3bd1137807ba94b1183a6bfe05ea7aa82d9e
alexander-poon <alexander.poon@vanderbilt.edu>
2017-09-26 18:49:48 -0500

    use round5 in amo_target

--- R/amo_target.R
+++ R/amo_target.R
@@ -18,7 +18,7 @@ amo_target <- function(n, pct, double = FALSE) {
         denom <- 8
     }
 
-    ifelse(n >= 30, round(pct + (100 - pct)/denom, 1), NA_real_)
+    ifelse(n >= 30, round5(pct + (100 - pct)/denom, 1), NA_real_)
 }
 
 amo_reduction <- function(n, pct, double = FALSE) {
