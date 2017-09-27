97df175f1c5329de1191ff6d5b4e332f06a99456
alexander-poon <alexander.poon@vanderbilt.edu>
2017-09-27 11:10:58 -0500

    Add View_ functions

---
+++ R/View_at_if.R
@@ -0,0 +1,28 @@
+#' View a subset of a data frame.
+#'
+#' @param df A data frame.
+#' @param ... A logical condition (View_if) or an unquoted set of variables (View_at).
+#'
+#' @export
+#'
+#' @examples
+#'
+#' df <- data.frame(
+#'     x = sample(LETTERS, 1000, replace = TRUE),
+#'     y = rnorm(1000),
+#'     z = rnorm(1000)
+#' )
+#'
+#' View_if(df, x == "A")
+#'
+#' View_at(df, x, z)
+#'
+View_at <- function(df, ...) {
+    dplyr::select(df, ...) %>%
+        View()
+}
+
+View_if <- function(df, ...) {
+    dplyr::filter(df, ...) %>%
+        View()
+}
